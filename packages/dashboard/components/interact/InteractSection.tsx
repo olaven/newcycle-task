bc5ed13b4c45f212d94c5f7df85396978f8e00c5
olaven <olav@sundfoer.com>
2023-02-07 14:52:29 +0100

    removed TODO From GUI

--- packages/dashboard/components/interact/InteractSection.tsx
+++ packages/dashboard/components/interact/InteractSection.tsx
@@ -4,9 +4,6 @@ import { TransferItems } from "./TransferItems";
 export const InteractSection = () => (
   <div>
     <CreateItems />
-    <div style={{ backgroundColor: "green", color: "white" }}>
-      TODO transfer an item between two users
-    </div>
     <TransferItems />
     <h3>Transfer Item between two users</h3>
   </div>
